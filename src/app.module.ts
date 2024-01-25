65b754dd92d36480f7d261d49be90a99e2bf7503
Shujaat Azim <shujaat.azim@gmail.com>
2024-01-24 21:53:09 -1000

    feat: Create resources for collection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
+import { CollectionModule } from './collection/collection.module';
 
 @Module({
-  imports: [CardsModule],
+  imports: [CardsModule, CollectionModule],
   controllers: [AppController],
   providers: [AppService],
 })
