9a1fb9c2e056bf9ae65a64733317537618112d85
Shujaat Azim <shujaat.azim@gmail.com>
2024-01-31 00:29:58 -1000

    feat: Recreate resource for users with CRUD endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
 import { CollectionModule } from './collection/collection.module';
 import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [CardsModule, CollectionModule, UsersModule],
