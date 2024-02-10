90a46870da21638ad256c5a654014bd5bcb033eb
Shujaat Azim <shujaat.azim@gmail.com>
2024-02-09 20:37:54 -1000

    feat: Build out Create Card DTO.

--- src/cards/dto/create-card.dto.ts
+++ src/cards/dto/create-card.dto.ts
@@ -1,1 +1,7 @@
-export class CreateCardDto {}
+export class CreateCardDto {
+  readonly id: number;
+  readonly number: number;
+  readonly name: string;
+  readonly set: string;
+  readonly rarity: 'secret' | 'holo' | 'rare' | 'uncommon' | 'common';
+}
