5019a56be61996d7d412940728ca9e4655eb8ead
Shujaat Azim <shujaat.azim@gmail.com>
2024-02-20 15:02:59 -0500

    feat: Add argon, class transformer and validator packages. Build out registration logic and dto.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { CardsModule } from './cards/cards.module';
 import { CollectionModule } from './collection/collection.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CardsModule, CollectionModule, UsersModule, AuthModule],
+  imports: [
+    CardsModule,
+    CollectionModule,
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   login() {
     return { status: 'Success', message: 'Successful login' };
   }
 
-  register() {
-    return { status: 'Success', message: 'Successful registration' };
+  async register(dto: AuthDto) {
+    // generate password hash
+    // save new user in db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: await argon.hash(dto.password),
+        username: dto.username,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        createdAt: true,
+      },
+    });
+    // return newly created and saved user
+    return { status: 'Success', message: 'Successful registration', ...user };
   }
 }
