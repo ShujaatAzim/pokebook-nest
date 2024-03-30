e4fbf158b08a94f5f5501349f644c98fe21c23c8
Shujaat Azim <shujaat.azim@gmail.com>
2024-03-30 01:25:57 -0400

    test: Adjust controller tests, set up for future testing.

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,23 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaService } from '../prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
+import { AuthDto } from './dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
+
+  const mockService = {
+    register: jest.fn(),
+  };
+
+  const mockAuthDto: AuthDto = {
+    email: 'zug@loktar.com',
+    password: '12345',
+    username: 'garrosh',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, PrismaService, JwtService, ConfigService],
+      providers: [{ provide: AuthService, useValue: mockService }],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Register', () => {
+    it('should call the register function in the service', async () => {
+      await controller.register(mockAuthDto);
+      expect(mockService.register).toHaveBeenCalledWith(mockAuthDto);
+    });
   });
 });
