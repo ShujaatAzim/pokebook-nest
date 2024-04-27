9619e0c6d20fd987429a332c04c7de9421ee615c
Shujaat Azim <shujaat.azim@gmail.com>
2024-04-27 12:56:55 -0400

    refactor: Update working message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   isWorking(): string {
-    return 'Pokébook Backend';
+    return 'Pokébook Backend is running.';
   }
 }
